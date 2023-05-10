da0fbd7f1f7d28ae2914527e367121de484969a3
Minh Vo <quangminhvo79@gmail.com>
2023-05-10 22:30:37 +0700

    Implement User Registration and Login

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+if Rails.env.development?
+  admin = User.where(email: 'admin@gmail.com').first_or_initialize
+  admin.password = '123456'
+  admin.save
+end
